4dc5e6a48622dbada20bfc907896010c0e885cec
Zhonggang Li <lizhonggang80@gmail.com>
2020-05-09 21:47:16 -0500

    Add files via upload

---
+++ db/migrate/20200509171230_create_admins.rb
@@ -0,0 +1,10 @@
+class CreateAdmins < ActiveRecord::Migration[6.0]
+  def change
+    create_table :admins do |t|
+      t.string :username
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
